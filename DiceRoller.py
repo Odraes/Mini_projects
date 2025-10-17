b392ce1a324a0f88bd5b7a077f50c067e41eae55
user <jyslergeraldo@hotmail.com>
2025-10-18 01:34:01 +0800

    the user can specify how many dice they want to roll

--- DiceRoller.py
+++ DiceRoller.py
@@ -1,13 +1,20 @@
 import random
 
-while true:
+while True:
     print("roll the dice? (Y/N)")
     answer = input()
     answer = answer.upper()
+
     if answer == "Y":
-        die1 = random.randint(1, 9)
-        die2 = random.randint(1, 9)
-        print(f"the dice is ({die1}) ({die2})")
+        print("how many dice?")
+        diceNumber = input()
+        diceNumber = int(diceNumber)
+        for i in range(diceNumber):
+            die1 = random.randint(1, 9)
+            die2 = random.randint(1, 9)
+            print(f"the dice is ({die1}) ({die2})")
+
+
     elif answer == "N":
         print("thank you for playing")
         break
